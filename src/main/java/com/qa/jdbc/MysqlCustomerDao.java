dd3e5affbf1bb5490fea9dc93f63cac556223fc0
jawilliams24 <jwilliams9517@gmail.com>
2020-02-10 14:51:36 +0000

    Changed pom file

--- src/main/java/com/qa/jdbc/MysqlCustomerDao.java
+++ src/main/java/com/qa/jdbc/MysqlCustomerDao.java
@@ -47,12 +47,10 @@ public class MysqlCustomerDao implements Dao<Customer> {
 		return customers;
 	}
 
-	public void create(Customer t) {
+	public void create(Customer customer) {
 		try {
 			Statement smnt = conn.createStatement();
-			System.out.println("moo");
-//			smnt.executeQuery("INSERT INTO customers VALUES(null,'Sam','White')");
-			smnt.executeUpdate("INSERT INTO customers VALUES(null,'Sam','White');");
+			smnt.executeUpdate("INSERT INTO customers(first_name,surname) VALUES('" + customer.getFirstName() + "','" + customer.getSurname()+"');");
 			conn.close();
 
 		} catch (Exception e) {
@@ -61,16 +59,30 @@ public class MysqlCustomerDao implements Dao<Customer> {
 		}
 	}
 
-//	public ArrayList<Customer> readAll() {
-//		return null;
-//	}
 
 	public void update(Customer t) {
+		try {
+			
+			Statement smnt = conn.createStatement();
+			smnt.executeUpdate("UPDATE customers SET customer_name= where customer_name=;");
+			conn.close();
 
+		} catch (Exception e) {
+			System.err.println("Got an exception!");
+			System.err.println(e.getMessage());
+		}		
 	}
 
 	public void delete(int id) {
+		try {
+			Statement smnt = conn.createStatement();
+			smnt.executeUpdate("DELETE FROM customers WHERE ");
+			conn.close();
 
+		} catch (Exception e) {
+			System.err.println("Got an exception!");
+			System.err.println(e.getMessage());
+		}
 	}
 
 }
