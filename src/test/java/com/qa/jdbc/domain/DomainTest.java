c0bdbfe099e4c195e4b0157302052d260635b5b9
jawilliams24 <jwilliams9517@gmail.com>
2020-02-13 21:59:05 +0000

    tests changed again

--- src/test/java/com/qa/jdbc/domain/DomainTest.java
+++ src/test/java/com/qa/jdbc/domain/DomainTest.java
@@ -1,35 +1,35 @@
-//package com.qa.jdbc.domain;
-//
-//import static org.junit.Assert.assertTrue;
-//
-//import org.junit.Test;
-//
-//import com.qa.domain.Domain;
-//
-//public class DomainTest {
-//	
-//	@Test
-//	public void customerTest() {
-//		Domain domain = Domain.CUSTOMER;
-//		assertTrue(domain.getDescription().toLowerCase().contains("customer"));
-//	}
-//	
-//	@Test
-//	public void itemTest() {
-//		Domain domain = Domain.ITEM;
-//		assertTrue(domain.getDescription().toLowerCase().contains("item"));
-//	}
-//	
-//	@Test
-//	public void orderTest() {
-//		Domain domain = Domain.ORDER;
-//		assertTrue(domain.getDescription().toLowerCase().contains("items"));
-//	}
-//	
-//	@Test
-//	public void stopTest() {
-//		Domain domain = Domain.STOP;
-//		assertTrue(domain.getDescription().toLowerCase().contains("close"));
-//	}
-//
-//}
+package com.qa.jdbc.domain;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import com.qa.domain.Domain;
+
+public class DomainTest {
+	
+	@Test
+	public void customerTest() {
+		Domain domain = Domain.CUSTOMER;
+		assertTrue(domain.getDescription().toLowerCase().contains("customer"));
+	}
+	
+	@Test
+	public void itemTest() {
+		Domain domain = Domain.ITEM;
+		assertTrue(domain.getDescription().toLowerCase().contains("item"));
+	}
+	
+	@Test
+	public void orderTest() {
+		Domain domain = Domain.ORDER;
+		assertTrue(domain.getDescription().toLowerCase().contains("items"));
+	}
+	
+	@Test
+	public void stopTest() {
+		Domain domain = Domain.STOP;
+		assertTrue(domain.getDescription().toLowerCase().contains("close"));
+	}
+
+}
