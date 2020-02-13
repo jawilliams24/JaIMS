155670dea44d2117e96d7fec883ec8937b1ad1d4
jawilliams24 <jwilliams9517@gmail.com>
2020-02-13 11:47:07 +0000

    package-infos added

---
+++ src/main/java/com/qa/services/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This is the package for the Services classes, which allow the user to
+ * interact with the table in the database, based on their choice.
+ * 
+ * @author James Williams
+ *
+ */
+package com.qa.services;
