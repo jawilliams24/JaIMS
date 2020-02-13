f65062754f50984b5544469b2be1bf8deceedf9f
jawilliams24 <jwilliams9517@gmail.com>
2020-02-13 16:44:22 +0000

    issues fixed

--- src/main/java/com/qa/utils/Config.java
+++ src/main/java/com/qa/utils/Config.java
@@ -10,28 +10,28 @@ package com.qa.utils;
 
 public class Config {
 
-	public static String username;
-	public static String password;
-	public static String url = "jdbc:mysql://35.204.131.16:3306/inventory_management_sys";
+	private static String username;
+	private static String password;
+	private static String url = "jdbc:mysql://35.204.131.16:3306/inventory_management_sys";
 	
-//	public static String getUsername() {
-//		return username;
-//	}
-//	public static void setUsername(String username) {
-//		Config.username = username;
-//	}
-//	public static String getPassword() {
-//		return password;
-//	}
-//	public static void setPassword(String password) {
-//		Config.password = password;
-//	}
-//	public static String getUrl() {
-//		return url;
-//	}
-//	public static void setUrl(String url) {
-//		Config.url = url;
-//	}
+	public static String getUsername() {
+		return username;
+	}
+	public static void setUsername(String username) {
+		Config.username = username;
+	}
+	public static String getPassword() {
+		return password;
+	}
+	public static void setPassword(String password) {
+		Config.password = password;
+	}
+	public static String getUrl() {
+		return url;
+	}
+	public static void setUrl(String url) {
+		Config.url = url;
+	}
 	
 	
 }
